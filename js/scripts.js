703c2a4eaf5242af7ede21e72ff812a9d3d463a3
Ashlin Aronin & Samantha Maturen <student@epicodus.com>
2015-09-09 10:11:47 -0700

    Write tests for firstVowel function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 var pigLatin = function(message) {
-    return message + "ay";
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var firstChar = message.charAt(0);
+    var translatedMessage;
+
+
+    // If first char is a consonant, move it to the end and add "ay"
+    if (vowels.indexOf(firstChar) === -1) {
+
+        // If second char is a consonant...
+        if (vowels.indexOf(message.charAt(1)) === -1) {
+            translatedMessage = message.slice(2) + message.slice(0, 2) + "ay";
+        } else {
+            translatedMessage = message.slice(1) + firstChar + "ay";
+        }
+
+    }  else {
+        // If first char is not a consonant, add "ay"
+        translatedMessage = message + "ay";
+    }
+
+    return translatedMessage;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,27 @@
 describe('pigLatin', function() {
-  it("handles words that start with a vowel", function() {
-    expect(pigLatin("art")).to.equal("artay");
-  });
+    it("handles words that start with a vowel", function() {
+        expect(pigLatin("art")).to.equal("artay");
+    });
+
+    it("handles words that start with one consonant", function() {
+        expect(pigLatin("pig")).to.equal("igpay");
+    });
+
+    it("handles words that start with two consonants", function() {
+        expect(pigLatin("chat")).to.equal("atchay");
+    });
+});
+
+describe('firstVowel', function() {
+    it("handles a single-character word", function() {
+        expect(firstVowel("a")).to.equal(0);
+    });
+
+    it("handles a two-letter word", function() {
+        expect(firstVowel("an")).to.equal(0);
+    });
+
+    it("handles a three-letter word", function() {
+        expect(firstVowel("she")).to.equal(2);
+    });
 });
