0fb416a369c78f02c630b2588d5484934bef770e
Ashlin Aronin & Samantha Maturen <student@epicodus.com>
2015-09-09 11:24:26 -0700

    Make multiple words test pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,54 @@
-var pigLatin = function(message) {
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-    var translatedMessage;
-    var firstVowelIndex = firstVowel(message);
+var pigLatin = function(word) {
+    var translatedWord;
+    var firstVowelIndex = firstVowel(word);
 
 
     /* If the word starts with a vowel, just add "ay".
     ** If it starts with any number of consonants, add them to the end + "ay".*/
-    if (firstVowelIndex == 0) {
-        translatedMessage = message + "ay";
+    if (firstVowelIndex === 0) {
+        /* Add "yay" to single-vowel word. Add "ay" to longer word which starts
+        ** with a vowel. */
+        if (word.length === 1) {
+            translatedWord = word + "way";
+        } else {
+            translatedWord = word + "ay";
+        }
     } else {
-        translatedMessage = message.slice(firstVowelIndex) +
-            message.slice(0, firstVowelIndex) + "ay";
+        /* If we run into a 'qu' where the 'u' is the first vowel, include the
+        ** 'u' at the end of the word. Otherwise just move consonants to the
+        ** end and add "ay". */
+        if (word[firstVowelIndex] === 'u' && word[firstVowelIndex - 1] === "q") {
+            translatedWord = word.slice(firstVowelIndex + 1) +
+                word.slice(0, firstVowelIndex + 1) + "ay";
+        } else {
+            translatedWord = word.slice(firstVowelIndex) +
+                word.slice(0, firstVowelIndex) + "ay";
+        }
     }
-    return translatedMessage;
+
+    return translatedWord;
 };
 
 
 
-var firstVowel = function(message) {
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-    for (var i = 0; i < message.length; i++) {
+var firstVowel = function(word) {
+    var vowels = ['a', 'e', 'i', 'o', 'u', "A", "E", "I", "O", "U"];
+    for (var i = 0; i < word.length; i++) {
         // If we find a vowel at this position, return this index
-        if (vowels.indexOf(message[i]) !== -1) {
+        if (vowels.indexOf(word[i]) !== -1) {
             return i;
         }
     }
 
     // We didn't find a vowel in this word... return -1
     return -1;
+};
+
+var handleArray = function(message) {
+    var words = message.split(" ");
+    var translatedMessage = "";
+    words.forEach(function(word) {
+        translatedMessage += pigLatin(word) + " ";
+    });
+    return translatedMessage;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -10,6 +10,22 @@ describe('pigLatin', function() {
     it("handles words that start with two consonants", function() {
         expect(pigLatin("chat")).to.equal("atchay");
     });
+
+    it("handles words that start with 'qu'", function() {
+        expect(pigLatin("queen")).to.equal("eenquay");
+    });
+
+    it("handles words that start with a consonant and contain 'qu'", function() {
+        expect(pigLatin("squeal")).to.equal("ealsquay");
+    });
+
+    it("handles words that start with 'y'", function() {
+        expect(pigLatin("yes")).to.equal("esyay");
+    });
+
+    it("handles a single-letter word", function() {
+        expect(pigLatin("i")).to.equal("iway");
+    });
 });
 
 describe('firstVowel', function() {
@@ -27,5 +43,11 @@ describe('firstVowel', function() {
 
     it("handles a no-vowel word", function() {
         expect(firstVowel("thyrmldn")).to.equal(-1);
+    });
+});
+
+describe("handleArray", function() {
+    it("handles multiple word string", function() {
+        expect(handleArray("I can speak Pig Latin")).to.equal("Iway ancay eakspay igPay atinLay ");
     });
 });
